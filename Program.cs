b6ad62b091c66caef0997bd6ccfaba47ca31372b
Prakash Mandal <pm1447616@gmail.com>
2022-02-03 13:37:14 +0530

    Add : Commands

--- Program.cs
+++ Program.cs
@@ -11,36 +11,89 @@ namespace SampleAssistant
     {
         static void Main(string[] args)
         {
-             SpeechSynthesizer talk = new SpeechSynthesizer();
+            SpeechSynthesizer talk = new SpeechSynthesizer();
              talk.SelectVoiceByHints(VoiceGender.Female); 
-             talk.Speak("Hello Sir,"+Greet());
-            System.Threading.Thread.Sleep(1000);
+             talk.Speak("Hello Sir, "+Greet());
+            System.Threading.Thread.Sleep(500);
             talk.SpeakAsync("What would u like to do ?");
             Console.WriteLine("1. Movies");
             Console.WriteLine("2. Games");
             Console.WriteLine("3. Songs");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            string choice =Console.ReadLine();
             switch (choice)
             {
-                case 1:
+                case "1":
                     {
                         talk.SpeakAsync("Which movie u like to watch?");
                     }break;
-                case 2:
+                case "2":
                     {
                         talk.SpeakAsync("Which Game u like to Play?");
                     }
                     break;
-                case 3:
+                case "3":
                     {
                         talk.SpeakAsync("Booting up Spotify ");
                     }
                     break;
+                case "Nothing":
+                    {
+                        talk.SpeakAsync("Okay let's move on! ");
+                    }break;
                 default: { talk.SpeakAsync("I am not sure what u want to do!"); }break;
             }
 
-            Console.Read();
+            bool off = true;
+             boot:    //Restart point
+            
+            do
+            {
+                string getInput = Console.ReadLine();
+
+                string input = getInput.ToLower();
+                if (input.Contains("time"))
+                {
+                    talk.SpeakAsync(DateTime.Now.ToString("h:mm tt"));
+                }
+                else if (input.Contains("date"))
+                {
+                    talk.SpeakAsync(DateTime.Now.ToString("dd/MM/yyyy"));
+                }
+                else if (input.Contains("who are you") || input.Contains("tell me about yourself"))
+                {
+                    talk.SpeakAsync("My Name is Lacia. Virtual Assistant. Just a sample for now");
+                }
+                else if ((input.Contains("you") && input.Contains("go off") || input.Contains("lacia") && input.Contains("go off")) && Greet() == "Good Evening")
+                {
+                    talk.SpeakAsync("Going off, Good Night Sir");
+                    off= false;
+                    GC.Collect();
+                }
+                else if (input.Contains("you") && input.Contains("go off") || input.Contains("lacia") && input.Contains("go off"))
+                {
+                    talk.SpeakAsync("Going off, Have a nice day sir");
+                    off = false;
+                    GC.Collect();
+                }
+            } while(off);
+
+             restart:
+            string tempreboot = Console.ReadLine();
+            string reboot = tempreboot.ToLower();
+            if (reboot.Contains("lacia")&& (reboot.Contains("wakeup")|| reboot.Contains("wake up")))
+            {
+                RandomWelcome welcome = new RandomWelcome();  //random welcome speech
+                talk.SpeakAsync(welcome.Welcome() +",   how can i help you"); 
+                off = true;
+                goto boot;
+            }
+            else { goto restart; }
+            
+            
+            
         }
+
+        //Methods
         public static string Greet()
         {
             DateTime current = DateTime.Parse(DateTime.Now.ToString("h tt"));
---
+++ RandomWelcome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SampleAssistant
+{
+     class RandomWelcome
+    {
+        private Random welcome = new Random();
+        private int Rand { get; set; }
+        private string NewWelcome { get; set; }
+        public RandomWelcome()
+        {
+            Rand = welcome.Next(1,5);
+        }
+
+        public string Welcome()
+        {
+            if(Rand == 1)
+            {
+                NewWelcome = "Welcome back sir ";
+            }
+            else if (Rand == 2)
+            {
+                
+                NewWelcome = "Yeah  yeah i am up";
+            }
+            else if (Rand == 3)
+            {
+                NewWelcome = "OH you are back";
+            }
+            else if(Rand == 4)
+            {
+                NewWelcome = "I am always awake ";
+            }
+            return NewWelcome;
+        }
+        
+    }
+}
